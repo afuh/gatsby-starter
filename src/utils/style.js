064af0fe480d64275039fd1e9082a0bffbac9d2d
Axel <axelfuh@gmail.com>
2018-11-14 16:53:42 +0100

    update style

--- src/utils/style.js
+++ src/utils/style.js
@@ -1,42 +1,70 @@
-import styledNormalize from 'styled-normalize'
 import { css, createGlobalStyle } from 'styled-components'
 
-// Example palette Material 500
-// https://material.io/guidelines/style/color.html
-const palette = {
-  red: "#F44336",
-  pink: "#E91E63",
-  purple: "#9C27B0",
-  deeppurple: "#673AB7",
-  cyan: "#00BCD4",
-  indigo: "#3F51B5",
-  lightblue: "#03A9F4",
-  blue: "#2196F3",
-  green: "#4CAF50",
-  teal: "#009688",
-  orange: "#FF9800",
-  deeporange: "#FF5722",
-  brown: "#795548",
-  darkgray: "#4f4f4f",
-  bluegray: "#607D8B",
-  lightgreen: '#8BC34A',
-  lime: '#CDDC39',
-  yellow: '#FFEB3B',
-  amber: '#FFC107',
-  lightgray: '#d8d8d8',
-  gray: '#9E9E9E',
-  whitesmoke: '#fafafa'
-}
-
 export const theme = {
-  danger: palette.red,
-  active: palette.orange,
-  link: palette.blue,
-  background: palette.whitesmoke,
+  black: "#212129",
+  white: "#F9F9FA",
+  gray: "#9F9FA3",
+  deeporange: "#FF5722",
   shadow: '0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08)'
 }
 
-export const defaultFont = [
+const screenBreak = {
+  mobile: 992,
+  phone: 650,
+  small: 480
+}
+
+export const media = {
+  mobile: inner => css`
+    @media (max-width: ${screenBreak.mobile / 16}em) {
+      ${inner}
+    }
+  `,
+  phone: inner => css`
+    @media (max-width: ${screenBreak.phone / 16}em) {
+      ${inner}
+    }
+  `,
+  small: inner => css`
+    @media (max-width: ${screenBreak.small / 16}em) {
+      ${inner}
+    }
+  `,
+  custom: (n, inner) => css`
+    @media (max-width: ${n / 16}em) {
+      ${inner}
+    }
+  `
+}
+
+export const flex = opt => css`
+  display: flex;
+
+  flex-direction: ${opt.dir|| "row"};
+  justify-content: ${opt.x || "center"};
+  align-items: ${opt.y || "center"};
+`
+
+export const hover = inner => css`
+  &:hover,
+  &:focus {
+    ${inner}
+  }
+`
+
+export const fontS = size => css`
+  font-size: ${size}rem;
+
+  ${media.mobile(css`
+    font-size: ${size - (size/5)}rem;
+  `)}
+
+  ${media.phone(css`
+    font-size: ${size - (size/4)}rem;
+  `)}
+`
+
+const defaultFont = [
   '-apple-system',
   'BlinkMacSystemFont',
   '"Segoe UI"',
@@ -48,32 +76,39 @@ export const defaultFont = [
   'sans-serif'
 ].join()
 
-export const typography = css`
-  h1, h2, h3, h4, h5, h6 {
-    font-weight: 500;
-  }
+const typography = css`
+  h1 { ${fontS(3.2)}; }
+  h2 { ${fontS(2.4)}; }
+  h3 { ${fontS(1.9)}; }
 
   a {
     text-decoration: none;
-    color: ${theme.link};
+    color: ${theme.black};
 
       &:hover,
       &:active,
       &:focus {
         text-decoration: underline;
+        color: ${theme.deeporange};
       }
     }
 `
 
 export const GlobalStyle = createGlobalStyle`
-  ${styledNormalize}
-
   html {
+    box-sizing: border-box;
     font-size: 10px;
   }
 
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
   body {
+    padding: 0;
+    margin: 0;
     font-size: 1.5rem;
+    letter-spacing: 0.02rem;
     font-family: ${defaultFont};
   }
 
